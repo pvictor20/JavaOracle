6f1aaccf809ac2d5b64119d9c78d540414dd8cdd
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-09-30 20:43:58 -0300

    Susposta tentativa de uma carinha feliz

---
+++ ProblemSet2_1.java
@@ -0,0 +1,16 @@
+public class ProblemSet2_1 {
+    public static void main(String[] args) {
+        //Use 8 print statements to print a smiley face.
+        //Your art will rely on only a single character, besides space, such as X or #.
+        
+        System.out.println("   XXXXX   ");
+        System.out.println("  X     X  ");
+        System.out.println(" X  x x  X ");
+        System.out.println(" X   x   X ");
+        System.out.println(" X  x x  X ");
+        System.out.println("  X     X  ");
+        System.out.println("   XXXXX   ");
+        System.out.println("           ");
+    
+    }   
+}
