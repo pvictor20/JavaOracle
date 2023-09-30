4e3e80ce075074b8daab2d6fa02e91c824ce1b4c
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-09-30 20:49:50 -0300

    Tentativa de gato

---
+++ ProblemSet2_2.java
@@ -0,0 +1,21 @@
+public class ProblemSet2_2 {
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+        
+        System.out.println("\\      /\\");
+        System.out.println(" \\    /  \\");
+        System.out.println("  /\\ /    /\\");
+        System.out.println(" /  \\    /  \\");
+        System.out.println("(    '.__.'    )");
+        System.out.println(" \\             /");
+        System.out.println("  \\  (  )  (  )");
+        System.out.println("   \\   )(__)(   )");
+        System.out.println("     /         \\");
+        System.out.println("    /  /  |  \\  \\");
+        System.out.println("   /  /   |   \\  \\");
+        System.out.println("  /  /    |    \\  \\");
+        System.out.println(" (__)    (__)");
+        
+    } 
+}
