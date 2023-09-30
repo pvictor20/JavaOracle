06a7d639cdc630794e59328f72fed27703c0c832
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-09-30 20:09:09 -0300

    Exercicio da seção JFo 2-2

---
+++ Text02.java
@@ -0,0 +1,31 @@
+/*
+ * Arrumar o programa e produzir a saida.
+ * public class Text02 {
+
+    public static void main(String[] args) {
+        System.out.println("1");
+        {
+            System.out.println("2")
+        }
+        System.out.println("3");
+        }
+    System.out.println ("4");
+    }
+}
+
+ */
+public class Text02 {
+
+    public static void main(String[] args) {
+        System.out.println("1");
+        
+        System.out.println("2");
+        
+        System.out.println("3");
+        
+        System.out.println ("4");
+
+        System.out.println("Arrumado :D");
+    }
+}    
+
