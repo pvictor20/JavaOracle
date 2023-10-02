2cf7e18f4b44d4c0b55b24931bffe5bfbd68a3ee
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-10-02 11:55:41 -0300

    Exercicio de variaveis

--- Variables02.java
+++ Variables02.java
@@ -5,11 +5,20 @@ public class Variables02 {
         //Step 1) Change the print statements in lines 12 and 16 so that they print the value of x.
         //Step 2) Change the value of x to "kitty".
         String x = "puppy";
-        System.out.println("puppy");
+        System.out.println(x);
         
         //Step 3) Add a line of code that changes the value of x to "bunny".
         
         System.out.println("puppy");
+
+        x = "kitty";
+
+        System.out.println(x);
+        System.out.println("kitty");
+
+        x = "bunny";
+        System.out.println(x);
+        System.out.println("bunny");
         
     }
 }
