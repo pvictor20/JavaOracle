9ff82ff529ac857bc9a7273e6b371f50781294ca
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-10-09 14:50:15 -0300

    Imports

---
+++ AddImport.java
@@ -0,0 +1,30 @@
+/*
+ * Arrumar este codigo 
+ * 
+ * import java.util.Calendar;
+import java.util.Date;
+
+public class AddImport {
+
+    public static void main(String args[]) {
+        javax.swing.JLabel label = new javax.swing.JLabel("hello");
+
+    }
+
+}
+
+
+ */
+
+import javax.swing.JLabel;
+
+
+public class AddImport {
+
+    public static void main(String args[]) {
+         JLabel label = new javax.swing.JLabel("hello");
+         System.out.println(label);
+
+    }
+
+}
