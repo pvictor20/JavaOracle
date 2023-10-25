79b1e23da151e7d0e5fe2fd416ca75392d904dcb
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-10-25 00:37:57 -0300

    Adicionei dois novos exercicios nesse arquivo.

--- Countdown.java
+++ Countdown.java
@@ -2,12 +2,27 @@ public class Countdown {
 
     public static void main(String[] args) {
         
-        System.out.println("Contagem regressiva: ");
+        //System.out.println("Contagem regressiva: ");
 
-        for(int i = 10; i >= 0; i--) {
-            System.out.print(i +" "); 
-        }
+        //for(int i = 10; i >= 0; i--) {
+            //System.out.print(i +" "); 
+        //}
 
-        System.out.println("Decolar!");
+        //System.out.println("Decolar!");
+
+        //for (int i = 0; i <=5; i++) {
+            //System.out.println(i);
+        //}
+            int numero = 0;
+        for (int i = 0; i <= 20; i++) {
+
+            if (numero % 2 == 0) {
+                // Imprime o número
+                System.out.println(numero);
+            }
+            numero++;
+    }
+
+    
     }
 }
