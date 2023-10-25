a28c6e283da4a7ae1fd6201fdeb81a798e5981e9
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-10-25 00:32:52 -0300

    Exercicio de contagem regressiva usando FOR

---
+++ Countdown.java
@@ -0,0 +1,13 @@
+public class Countdown {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Contagem regressiva: ");
+
+        for(int i = 10; i >= 0; i--) {
+            System.out.print(i +" "); 
+        }
+
+        System.out.println("Decolar!");
+    }
+}
