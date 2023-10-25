28aa0a86cf0ea4ef86d7a0b4607b3421516b2689
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-10-25 00:49:50 -0300

    Contagem regressiva usando while

---
+++ ContagemRegressivaWhile.java
@@ -0,0 +1,13 @@
+public class ContagemRegressivaWhile {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println("Contagem Regressiva para o lançamento!");
+
+        while (i >=0) {
+            System.out.println(i);
+            i--;
+        }
+
+        System.out.println("Decolar!!");
+    }
+}
