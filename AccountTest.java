c596cf30ed7b2a69905f05249ce179a859bc9dcf
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-10-26 16:51:12 -0300

    uso de metodos

---
+++ AccountTest.java
@@ -0,0 +1,8 @@
+public class AccountTest {
+    public static void main(String[] args) {
+        contaPoupar cp = new contaPoupar();
+        cp.name = "Vito";
+        cp.deposito(1000);
+
+    }
+}
---
+++ contaPoupar.java
@@ -0,0 +1,8 @@
+public class contaPoupar {
+public String name;
+public double balance;
+
+public void deposito (int x) {
+    balance += x;
+}
+}
