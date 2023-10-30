219eac1cf293ff77c9a625f290ba1d480ed7f4a7
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-10-30 13:48:56 -0300

    Instanciando objeto

---
+++ PrisonTest.java
@@ -0,0 +1,8 @@
+public class PrisonTest {
+    public static void main(String[] args) {
+        Prisoner bubba = new Prisoner();
+        Prisoner twitch = new Prisoner();
+
+        System.out.println(bubba == twitch);
+    }
+}
---
+++ Prisoner.java
@@ -0,0 +1,9 @@
+public class Prisoner {
+    public String nome;
+    public double altura;
+    public int sentenca;
+
+    public void pensamento(){
+        System.out.println("Terei minha vingança!");
+    }
+}
