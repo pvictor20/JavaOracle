c9d3ca24a3178c58daf1d6a5f77a9f6158fc64d9
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-10-30 13:53:28 -0300

    Exemplificação de memoria heap e stack

--- PrisonTest.java
+++ PrisonTest.java
@@ -3,6 +3,16 @@ public class PrisonTest {
         Prisoner bubba = new Prisoner();
         Prisoner twitch = new Prisoner();
 
+        bubba.altura = 2.08;
+        bubba.nome = "Bubba";
+        bubba.sentenca = 4;
+
+        twitch.altura = 1.73;
+        twitch.nome = "Twitch";
+        twitch.sentenca = 3;
+
+        bubba = twitch; //Para ver tipo de memória heap e stack;
+
         System.out.println(bubba == twitch);
     }
 }
