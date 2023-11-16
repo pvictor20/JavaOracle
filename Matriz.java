883c9d4bf2a405f84c234f044683581d88b0aead
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-11-16 16:45:10 -0300

    Usando matriz

--- Matriz.java
+++ Matriz.java
@@ -1,6 +1,6 @@
 public class Matriz {
     public static void main(String[] args) {
-        int [] ages = new int[3];
+        /*int [] ages = new int[3];
         ages[0] = 19;
         ages[1] = 42;
         ages [3] = 50;
@@ -8,11 +8,15 @@ public class Matriz {
         String [] names = new String [3];
         names[0] = "Maria";
         names[1] = "Matheus";
-        names[3] = "Carlão";
+        names[3] = "Carlão";*/
 
         ///////////// outro tipo de declaração
 
         //String[] names2 = {"Maria", "Matheus", "Carlão"};
-        //int[] ages2 = {19, 42, 50};
+        int[] ages2 = {21, 42, 50};
+
+        int myAge = ages2[0];
+        System.out.println("Minha idade é " + ages2[0]);
+        System.out.println("Minha idade também é " + myAge);
     }
 }
