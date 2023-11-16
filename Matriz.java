0da3979c687bc216d7ef0a4b93f17837978d3a26
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-11-16 16:41:27 -0300

    Matriz

---
+++ Matriz.java
@@ -0,0 +1,18 @@
+public class Matriz {
+    public static void main(String[] args) {
+        int [] ages = new int[3];
+        ages[0] = 19;
+        ages[1] = 42;
+        ages [3] = 50;
+
+        String [] names = new String [3];
+        names[0] = "Maria";
+        names[1] = "Matheus";
+        names[3] = "Carlão";
+
+        ///////////// outro tipo de declaração
+
+        //String[] names2 = {"Maria", "Matheus", "Carlão"};
+        //int[] ages2 = {19, 42, 50};
+    }
+}
